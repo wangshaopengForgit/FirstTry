a64cbddd377af385d2bfb32d9416f3f11202fb91
wangshaopengForgit <15235120106@163.com>
2022-11-18 14:26:02 +0800

    测试

--- src/cn/itcast_04/MySleepThreadDemo.java
+++ src/cn/itcast_04/MySleepThreadDemo.java
@@ -3,7 +3,7 @@ package cn.itcast_04;
 public class MySleepThreadDemo {
     public static void main(String[] args) {
         MySleepThread m1 = new MySleepThread("王少鹏");
-        MySleepThread m2 = new MySleepThread("刘亦菲");
+        MySleepThread m2 = new MySleepThread("江小畔");
         m1.start();
         m2.start();
 
